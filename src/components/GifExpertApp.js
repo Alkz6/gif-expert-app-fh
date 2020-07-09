17058bc75158a2079afc26ff336e6673def16fc1
Alexis <jalexismtz.medina@gmail.com>
2020-07-08 20:16:00 -0500

    Curso de React por fernando herrera

---
+++ src/components/GifExpertApp.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { AddCategory } from './AddCategory';
+import { GifGrid } from './GifGrid';
+
+export const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+
+    const [categories, setCategories] = useState(['One Punch']);
+
+    // const handleAdd = () => {
+    //     // setCategories(['HunterXHunter', ...categories]);
+    //     setCategories(category => [...category, 'HunterXHunter']);
+    // }
+
+    return (
+        <>
+         <h1>GifExpertApp</h1>
+         <AddCategory setCategories={setCategories} />
+         <hr />
+         {/* <button onClick={handleAdd}>Agregar</button> */}
+         <ol>
+             {
+                categories.map(category => (
+                    <GifGrid key={category} category={category} />
+                ))
+             }
+
+         </ol>
+        </>
+    )
+}
+
+
+// export default GifExpertApp;
