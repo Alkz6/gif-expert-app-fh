6f80b13bc7248d6549c6116ceadcf539567f8113
Alexis <jalexismtz.medina@gmail.com>
2020-07-08 20:17:14 -0500

    se cambia carpeta build a docs

---
+++ docs/precache-manifest.1d5f641bc2aa1e6770553848c3f79136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7072310aec02f5607ee84ff0a1b03b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a41b0c2e02b8f9a3145",
+    "url": "/static/css/main.f6edc470.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a41b0c2e02b8f9a3145",
+    "url": "/static/js/main.29753288.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
